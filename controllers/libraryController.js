7146013bbd81301f81238b228605532f823ea143
Léora Chriqui <leora.chriqui@gmail.com>
2025-06-06 13:59:01 +0200

    fixing library

--- controllers/libraryController.js
+++ controllers/libraryController.js
@@ -2,22 +2,22 @@ import Library from "../models/Library.js";
 
 export const getAllLibraries = async (req, res) => {
     try {
-        const Libraries = await Library.find();
-        res.status(200).json(books);
+        const libraries = await Library.find();
+        res.status(200).json(libraries);
     } catch (error) {
         console.error("Error fetching libraries:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 }
 
-export const getLibrarykById = async (req, res) => {
+export const getLibraryById = async (req, res) => {
     const { id } = req.params;
     try {
         const library = await Library.findById(id);
-        if (!Library) {
+        if (!library) {
             return res.status(404).json({ message: "Library not found" });
         }
-        res.status(200).json(Library);
+        res.status(200).json(library);
     }
     catch (error) {
         console.error("Error fetching Library:", error);
@@ -27,26 +27,20 @@ export const getLibrarykById = async (req, res) => {
 
 export const createLibrary = async (req, res) => {
     const {
-        title,
+        name,
         address, 
-        books : {
-            stock, 
-            bookRef,
-        }
+        books
     } = req.body;
     try {
         const newLibrary = new Library({ 
-            title,
+            name,
             address, 
-            books : {
-                stock, 
-                bookRef,
-            }   
+            books  
         });
         await newLibrary.save();
         res.status(201).json(newLibrary);
     } catch (error) {
-        console.error("Error creating book:", error);
+        console.error("Error creating library:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 };
@@ -54,22 +48,16 @@ export const createLibrary = async (req, res) => {
 export const updateLibrary = async (req, res) => {
     const { id } = req.params;
     const {
-        title,
-        address, 
-        books : {
-            stock, 
-            bookRef,
-        } 
+        name,
+        address,
+        books
     } = req.body;
 
     try {
         const updatedLibrary = await Library.findByIdAndUpdate(id, {
             title,
             address, 
-            books : {
-                stock, 
-                bookRef,
-            } 
+            books
         }, { new: true });
 
         if (!updatedLibrary) {
